681826b15ea1821541025c1d9c6d8ba10425476a
Harish <harishbommakanti2002@gmail.com>
2019-09-11 16:22:20 -0500

    app is able to read newSeason input
    - #navBtn --> #buttons to serve for all buttons
    - basics of seasonConfig page are set up but need a way to handle all of the season names

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//navbar on all pages//
 //open navbar
 function openNav(){
     document.getElementById("mySidenav").style.width= "250px";
@@ -6,4 +7,14 @@ function openNav(){
 //closing navbar
 function closeNav(){
 document.getElementById("mySidenav").style.width="0";
+}
+
+//season config js//
+//adds new season from input box
+function addNewSeason()
+{
+    var newSeason = document.getElementsByName("newSeasonInputBox")[0].value;
+    console.log("newSeason: ",newSeason);
+    document.getElementById("newSeasonConfirmation").innerHTML=
+    "Season " + "\'"+ newSeason +"\'"+" has been added.";
 }
