a6ea10cae6edc7c8d900c8c1ab3854c0ef22b99f
Harish <harishbommakanti2002@gmail.com>
2019-09-12 17:55:15 -0500

    only one radio button can be checked
    - now, for the part when one checks a radio button, only one radio can be checked in total.
    - need to work on storing the seasons on the page even after going back and forth between pages (loadSeasons() doesn't work, maybe cookies don't work when testing local html files)

--- script.js
+++ script.js
@@ -15,27 +15,52 @@ var seasonList = [];
 function addNewSeason()
 {
     var newSeason = document.getElementsByName("newSeasonInputBox")[0].value;
-    if (seasonList.includes(newSeason))
+    if (seasonList.includes(newSeason)) //if season is already stored
     {
         document.getElementById("newSeasonConfirmation").innerHTML=
     "<i>Season \'" + newSeason +"\' already exists.</i>";    
     } else
     {
+        //add to radioList
         seasonList.push(newSeason);
         document.getElementById("newSeasonConfirmation").innerHTML=
         "<i>Season " + "\'"+ newSeason +"\'"+" has been added.</i>";
-        displaySeasons(newSeason);
+        var formSpace = document.getElementById("seasonRadioList");
+        formSpace.innerHTML += "<input type=\"radio\" name=\"seasonListItem\"> "
+        + newSeason + "<br>";
+
+        //save newSeason in cookie
+        setCookie("seasonName",newSeason);
     }
 }
 
-//displays seasons in a radio list
-function displaySeasons(newSeason)
+function setCookie(name, value)
 {
-    var divSpace = document.getElementById("seasonRadioList");
-    divSpace.innerHTML += "<input type=\"radio\" class=\"seasonList\"> "
-    + newSeason + "<br>";
+    document.cookie = name + "=" + value + ";";
 }
 
+function getCookie(name)
+{
+    var re = new RegExp(name + "=([^;]+)");
+    var value = re.exec(document.cookie);
+    return (value != null) ? unescape(value[1]) : null;
+}
+
+//meant for the startup of the page, not when a new season gets displayed
+function loadSeasons()
+{
+    var formSpace = document.getElementById("seasonRadioList");
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var cookieList = decodedCookie.split(";");
+    for (var i = 0; i < cookieList.length; i++)
+    {
+        var season = cookieList[i].split("=")[1];
+        formSpace.innerHTML += "<input type=\"radio\" name=\"seasonListItem\"> "
+        + season + "<br>";
+    }
+}
+
+//clears all seasons from formspace
 function clearSeasons()
 {
     var divSpace = document.getElementById("seasonRadioList");
