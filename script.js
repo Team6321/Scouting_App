b28a9a08a09fcc35b7787a716144f18a01865987
Harish Bommakanti <33380236+harishbommakanti@users.noreply.github.com>
2019-09-11 23:11:19 -0500

    site is able to keep track of multiple seasons
    - added seasonList and created methods to prevent duplicate seasons
    - added a clearSeason button and added a "continue to event page" button
    - need to work on passing the selected season to the event page

--- script.js
+++ script.js
@@ -11,10 +11,34 @@ document.getElementById("mySidenav").style.width="0";
 
 //season config js//
 //adds new season from input box
+var seasonList = [];
 function addNewSeason()
 {
     var newSeason = document.getElementsByName("newSeasonInputBox")[0].value;
-    console.log("newSeason: ",newSeason);
-    document.getElementById("newSeasonConfirmation").innerHTML=
-    "Season " + "\'"+ newSeason +"\'"+" has been added.";
+    if (seasonList.includes(newSeason))
+    {
+        document.getElementById("newSeasonConfirmation").innerHTML=
+    "<i>Season \'" + newSeason +"\' already exists.</i>";    
+    } else
+    {
+        seasonList.push(newSeason);
+        document.getElementById("newSeasonConfirmation").innerHTML=
+        "<i>Season " + "\'"+ newSeason +"\'"+" has been added.</i>";
+        displaySeasons(newSeason);
+    }
+}
+
+//displays seasons in a radio list
+function displaySeasons(newSeason)
+{
+    var divSpace = document.getElementById("seasonRadioList");
+    divSpace.innerHTML += "<input type=\"radio\" class=\"seasonList\"> "
+    + newSeason + "<br>";
+}
+
+function clearSeasons()
+{
+    var divSpace = document.getElementById("seasonRadioList");
+    seasonList = [];
+    divSpace.innerHTML = " ";
 }
