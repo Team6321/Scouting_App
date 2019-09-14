d6492c4db877c1fb10af0a1fb9b771e7c0e2b59b
Harish Bommakanti <33380236+harishbommakanti@users.noreply.github.com>
2019-09-14 14:56:39 -0500

    seasonNames cookies is now regular
    - last few commits, doc.cookie was outputting irregular strings. now the doc.cookie is outputting a good semi-colon seperated value string
    - need to work on the clear seasons button clearing all the cookies next.
    - should also move the 'continue on to the event data' to the bottom page after questionnaire code

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //credit to w3schools for js tutorials
 
-//navbar on all pages//
+//**********navbar on all pages*************//
 //open navbar
 function openNav(){
     document.getElementById("mySidenav").style.width= "250px";
@@ -11,34 +11,76 @@ function closeNav(){
 document.getElementById("mySidenav").style.width="0";
 }
 
-//season config js//
+
+
+
+//***********season config js***************//
+
 //adds new season from input box
-var seasonList = [];
 function addNewSeason()
 {
-    var newSeason = document.getElementsByName("newSeasonInputBox")[0].value;
-    if (seasonList.includes(newSeason)) //if season is already stored
+    var newSeason = document.getElementsByName("newSeasonInputBox")[0].value.toString();
+
+    var cookieList = document.cookie.split(";");
+    var isAlreadyStored = false;
+    if (document.cookie.length > 0)
+    {
+        for (var i=0;i<cookieList.length;i++)
+        {
+            var val = cookieList[i].split("=")[1];
+            if (newSeason==val.toString())
+            {
+                isAlreadyStored = true;
+                break;
+            }
+        }
+    }
+
+    if (isAlreadyStored) //if season is already stored
     {
         document.getElementById("newSeasonConfirmation").innerHTML=
-    "<i>Season \'" + newSeason +"\' already exists.</i>";    
+        "<i>Season \'" + newSeason +"\' already exists.</i>";    
     } else
     {
         //add to radioList
-        seasonList.push(newSeason);
         document.getElementById("newSeasonConfirmation").innerHTML=
-        "<i>Season " + "\'"+ newSeason +"\'"+" has been added.</i>";
+        "<i>Season " + "\'"+ newSeason +"\'"+" has been added.</i>"; //confirmation text
         var formSpace = document.getElementById("seasonRadioList");
-        formSpace.innerHTML += "<input type=\"radio\" name=\"seasonListItem\"> "
+        formSpace.innerHTML += "<input type=\"radio\" name=\"seasonListItem\"> " //add to radiolist
         + newSeason + "<br>";
 
         //save newSeason in cookie
-        setCookie("seasonName",newSeason);
+        document.cookie = "season:" + newSeason + "=" + newSeason + "; expires=Wed, 1 Jan 2038 12:00:00 UTC; path=/";
     }
 }
 
-function setCookie(name, value)
+//meant for only the startup of the page
+function loadSeasons()
 {
-    document.cookie += name + "=" + value + ";";
+    var formSpace = document.getElementById("seasonRadioList");
+    if (document.cookie.length != 0)
+    {
+        var cookieList = document.cookie.split(";");
+        for (var i=0;i<cookieList.length;i++)
+        {
+            var cookieVal = cookieList[i].split("=")[1];
+            formSpace.innerHTML += "<input type=\"radio\" name=\"seasonListItem\"> "
+            + cookieVal + "<br>";
+        }
+    }
+}
+
+//clears all seasons from formspace
+function clearSeasons()
+{
+    console.log("doc.cookie: " + document.cookie);
+    //var divSpace = document.getElementById("seasonRadioList");
+    //divSpace.innerHTML = " ";
+
+    //clearing all cookies
+    var cookies = document.cookie.split(";");
+    var x = document.cookie.toString();
+    console.log(x);
 }
 
 function getCookie(cname)
@@ -67,23 +109,3 @@ function getCookie(name)
     var value = re.exec(document.cookie);
     return (value != null) ? unescape(value[1]) : null;
 }*/
-
-//meant for only the startup of the page
-function loadSeasons()
-{
-    var formSpace = document.getElementById("seasonRadioList");
-    var cookieList = document.cookie.split("seasonName=");
-    for (var i=1;i<cookieList.length;i++)
-    {
-        formSpace.innerHTML += "<input type=\"radio\" name=\"seasonListItem\"> "
-        + cookieList[i] + "<br>";
-    }
-}
-
-//clears all seasons from formspace
-function clearSeasons()
-{
-    var divSpace = document.getElementById("seasonRadioList");
-    seasonList = [];
-    divSpace.innerHTML = " ";
-}
