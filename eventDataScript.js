5a8a8a65990a075424590d51e6eac6570a7464af
Harish Bommakanti <33380236+harishbommakanti@users.noreply.github.com>
2019-11-06 13:01:50 -0600

    minor changes
    - some text changes on the html pages

--- eventDataScript.js
+++ eventDataScript.js
@@ -27,7 +27,7 @@ function showTopText()
         finalText = 'Here, enter all the events that the team will attend for season "' + currSeason + '".';
     } else
     {
-        finalText = 'No season is currently saved. Go back to the Season Configuration page ' + 
+        finalText = 'No season is currently checked. Go back to the Season Configuration page ' + 
         'to enter new seasons and season-specific elements/pit scouting questions. Make sure you check the new season you enter.'
     }
     $("#eventDataIntroText").text(finalText);
